08d813677bc2f68989871e289082033faa5b0df0
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-03-23 17:40:32 +0100

    feat: exercise done with bonus.

---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script></script>
+
+<template>
+    <div id="container-image">
+        <img src="/img/01.webp" alt="Spiderman">
+    </div>
+</template>
+
+<style>
+#container-image {
+    margin: 0 auto;
+    max-width: 600px;
+    max-height: 600px;
+}
+
+img {
+    width: 100%;
+}
+</style>
